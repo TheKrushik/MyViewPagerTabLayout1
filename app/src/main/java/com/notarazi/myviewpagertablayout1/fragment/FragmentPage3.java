82f7487db9b4ff80f1eec7c342ddf2f6a5e34c87
KrusH <krushik@ukr.net>
2016-11-28 01:03:47 +0200

    dialog

--- app/src/main/java/com/notarazi/myviewpagertablayout1/fragment/FragmentPage3.java
+++ app/src/main/java/com/notarazi/myviewpagertablayout1/fragment/FragmentPage3.java
@@ -5,13 +5,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.notarazi.myviewpagertablayout1.R;
 
 public class FragmentPage3 extends Fragment {
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_page3, null);
+        View view = inflater.inflate(R.layout.fragment_page3, null);
+
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Toast", Toast.LENGTH_SHORT).show();
+
+                VoteDialogFragment dialog = new VoteDialogFragment();
+                dialog.show(getFragmentManager(), "fragmentDialog");
+
+            }
+        });
+        return view;
     }
 }
---
+++ app/src/main/java/com/notarazi/myviewpagertablayout1/fragment/VoteDialogFragment.java
@@ -0,0 +1,29 @@
+package com.notarazi.myviewpagertablayout1.fragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+
+public class VoteDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Оцените приложение")
+                .setPositiveButton("Оценить", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("Позже", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
